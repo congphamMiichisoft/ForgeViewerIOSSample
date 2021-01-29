bc13884ae65b40442d832c7172936867a1b63e37
Công Phạm <congp@miichisoft.com>
2021-01-29 16:48:50 +0700

    add models

--- IOSVIEWEROFFLINE/TeleViewController.swift
+++ IOSVIEWEROFFLINE/TeleViewController.swift
@@ -11,40 +11,30 @@ import WebKit
 import MobileCoreServices
 class TeleViewController: UIViewController, UIDocumentPickerDelegate {
     var html : String!
-    func fetchImage(with imageName: String)  {
-        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
-        if let path = paths.first {
-            let imageURL = URL(fileURLWithPath: path).appendingPathComponent(imageName)
-           print("Fetch: ",imageURL)
-        }
-        
-    }
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-        print(urls.first)
-        webview.evaluateJavaScript("viewer.loadModel('\(urls.first?.absoluteString)')")
-        webview.reload()
-    }
-    func configButton(){
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Load", style: .done, target: self, action: #selector(leftAction))
-    }
     var tele : TelegraphDemo!
     var webview : WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        webview = WKWebView(
+            frame: self.view.bounds,
+            configuration: WKWebViewConfiguration()
+        )
+        view.addSubview(webview)
         let path = Bundle.main.path(forResource: "www/index", ofType: "html")
-         html = try! String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+        html = try! String(contentsOfFile: path!, encoding: String.Encoding.utf8)
         copyFolders()
-        configButton()
         tele = TelegraphDemo()
         tele.start()
-//        loadWebView()
+        loadModel()
+          
+    }
+    func loadModel(){
         var listURL = [URL]()
         let fileMan = FileManager.default
         let docPath = fileMan.urls(for: .documentDirectory, in: .userDomainMask)
         let modelPath = docPath[0].appendingPathComponent("www/models/", isDirectory: true)
         do {
-        let directoryContents = try FileManager.default.contentsOfDirectory(at: modelPath, includingPropertiesForKeys: nil, options: [])
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: modelPath, includingPropertiesForKeys: nil, options: [])
             let subdirs = directoryContents.filter{ $0.hasDirectoryPath }
             let name = subdirs.first?.path.replacingOccurrences(of: modelPath.path, with: "")
             for item in subdirs {
@@ -63,72 +53,41 @@ class TeleViewController: UIViewController, UIDocumentPickerDelegate {
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        
-      
-        
     }
-    @objc func leftAction(){
-        let importMenu = UIDocumentPickerViewController(documentTypes: [String(kUTTypeText),String(kUTTypeContent),String(kUTTypeItem),String(kUTTypeData)], in: .import)
-            importMenu.delegate = self
-            importMenu.modalPresentationStyle = .formSheet
-            self.present(importMenu, animated: true, completion: nil)
-    }
+    
     func loadWebView(name : String) {
         let htmlString = html.replacingOccurrences(of: "$$$path", with: name)
-        print("File chọn : \(name)")
-        webview = WKWebView(
-            frame: self.view.bounds,
-            configuration: WKWebViewConfiguration()
-        )
-        view.addSubview(webview)
-        webview.navigationDelegate = self
-        
         guard let url = URL(string: "http://localhost:8080/www") else { return }
         webview.loadHTMLString(htmlString, baseURL: url)
-//        webview.load(URLRequest(url: url))
-       
-        
     }
     func copyFolders() {
         let fileManager = FileManager.default
-
         let documentsUrl = fileManager.urls(for: .documentDirectory,
                                             in: .userDomainMask)
-
         guard documentsUrl.count != 0 else {
-            return // Could not find documents URL
+            return
         }
-
         let finalDatabaseURL = documentsUrl.first!.appendingPathComponent("www")
-
-//        if !( (try? finalDatabaseURL.checkResourceIsReachable()) ?? false) {
-            print("DB does not exist in documents folder")
-
-            let documentsURL = Bundle.main.resourceURL?.appendingPathComponent("www")
-
-            do {
-                if !FileManager.default.fileExists(atPath:(finalDatabaseURL.path))
-                {
-                    try FileManager.default.createDirectory(atPath: (finalDatabaseURL.path), withIntermediateDirectories: false, attributes: nil)
-                }
-                copyFiles(pathFromBundle: (documentsURL?.path)!, pathDestDocs: finalDatabaseURL.path)
-            } catch let error as NSError {
-                print("Couldn't copy file to final location! Error:\(error.description)")
+        print("DB does not exist in documents folder")
+        let documentsURL = Bundle.main.resourceURL?.appendingPathComponent("www")
+        do {
+            if !FileManager.default.fileExists(atPath:(finalDatabaseURL.path))
+            {
+                try FileManager.default.createDirectory(atPath: (finalDatabaseURL.path), withIntermediateDirectories: false, attributes: nil)
             }
-
-//        } else {
-//            print("Database file found at path: \(finalDatabaseURL.path)", "Bundle : \(Bundle.main)")
-//
-//        }
-
+            copyFiles(pathFromBundle: (documentsURL?.path)!, pathDestDocs: finalDatabaseURL.path)
+        } catch let error as NSError {
+            print("Couldn't copy file to final location! Error:\(error.description)")
+        }
+        
     }
-
+    
     func copyFiles(pathFromBundle : String, pathDestDocs: String) {
         let fileManagerIs = FileManager.default
         do {
             let filelist = try fileManagerIs.contentsOfDirectory(atPath: pathFromBundle)
             try? fileManagerIs.copyItem(atPath: pathFromBundle, toPath: pathDestDocs)
-
+            
             for filename in filelist {
                 try? fileManagerIs.copyItem(atPath: "\(pathFromBundle)/\(filename)", toPath: "\(pathDestDocs)/\(filename)")
             }
@@ -136,11 +95,5 @@ class TeleViewController: UIViewController, UIDocumentPickerDelegate {
             print("\nError\n")
         }
     }
-   
+    
 }
-extension TeleViewController: WKNavigationDelegate{
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        
-    }
-}
-
--- IOSVIEWEROFFLINE/TelegraphDemo.swift
+++ IOSVIEWEROFFLINE/TelegraphDemo.swift
@@ -31,111 +31,12 @@ extension TelegraphDemo {
       server = Server()
     }
 
-    // Set the delegates and a low web socket ping interval to demonstrate ping-pong
-    server.delegate = self
-    server.webSocketDelegate = self
     server.webSocketConfig.pingInterval = 10
-
-    // Define the demo routes
-    // Note: we're ignoring possible strong retain cycles in the demo
-    
-
-//    server.serveBundle(.main, "/")
     let fileManger = FileManager.default
     let urlDoc = fileManger.urls(for: .documentDirectory, in: .userDomainMask)
     server.serveDirectory(urlDoc[0])
-    // Handle up to 4 requests simultaneously
     server.concurrency = 4
-
-    // Start the server on localhost
-    // Note: we'll skip error handling in the demo
     try! server.start(port: 8080, interface: "localhost")
-
-    // Log the url for easy access
-    print("[SERVER]", "Server is running - url:", serverURL())
   }
 }
 
-
-// MARK: - ServerDelegate implementation
-extension TelegraphDemo: ServerDelegate {
-  // Raised when the server gets disconnected.
-  public func serverDidStop(_ server: Server, error: Error?) {
-    print("[SERVER]", "Server stopped:", error?.localizedDescription ?? "no details")
-  }
-}
-
-// MARK: - ServerWebSocketDelegate implementation
-extension TelegraphDemo: ServerWebSocketDelegate {
-  /// Raised when a web socket client connects to the server.
-  public func server(_ server: Server, webSocketDidConnect webSocket: WebSocket, handshake: HTTPRequest) {
-    let name = handshake.headers["X-Name"] ?? "stranger"
-    print("[SERVER]", "WebSocket connected - name:", name)
-
-    webSocket.send(text: "Welcome client \(name)")
-    webSocket.send(data: Data([0x00, 0x01, 0x02, 0x03, 0x04, 0x05]))
-  }
-
-  /// Raised when a web socket client disconnects from the server.
-  public func server(_ server: Server, webSocketDidDisconnect webSocket: WebSocket, error: Error?) {
-    print("[SERVER]", "WebSocket disconnected:", error?.localizedDescription ?? "no details")
-  }
-
-  /// Raised when the server receives a web socket message.
-  public func server(_ server: Server, webSocket: WebSocket, didReceiveMessage message: WebSocketMessage) {
-    print("[SERVER]", "WebSocket message received:", message)
-  }
-
-  /// Raised when the server sends a web socket message.
-  public func server(_ server: Server, webSocket: WebSocket, didSendMessage message: WebSocketMessage) {
-    print("[SERVER]", "WebSocket message sent:", message)
-  }
-}
-
-// MARK: - URLSessionDelegate implementation
-extension TelegraphDemo: URLSessionDelegate {
-  public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge,
-                         completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-    // Use our custom TLS policy to verify if the server should be trusted
-    let credential = tlsPolicy!.evaluateSession(trust: challenge.protectionSpace.serverTrust)
-    completionHandler(credential == nil ? .cancelAuthenticationChallenge : .useCredential, credential)
-  }
-}
-
-// MARK: - WebSocketClientDelegate implementation
-extension TelegraphDemo: WebSocketClientDelegate {
-  /// Raised when the web socket client has connected to the server.
-  public func webSocketClient(_ client: WebSocketClient, didConnectToHost host: String) {
-    print("[CLIENT]", "WebSocket connected - host:", host)
-  }
-
-  /// Raised when the web socket client received data.
-  public func webSocketClient(_ client: WebSocketClient, didReceiveData data: Data) {
-    print("[CLIENT]", "WebSocket message received - data:", data as NSData)
-  }
-
-  /// Raised when the web socket client received text.
-  public func webSocketClient(_ client: WebSocketClient, didReceiveText text: String) {
-    print("[CLIENT]", "WebSocket message received - text:", text)
-  }
-
-  /// Raised when the web socket client disconnects. Provides an error if the disconnect was unexpected.
-  public func webSocketClient(_ client: WebSocketClient, didDisconnectWithError error: Error?) {
-    print("[CLIENT]", "WebSocket disconnected - error:", error?.localizedDescription ?? "no error")
-  }
-}
-
-// MARK: Request helpers
-extension TelegraphDemo {
-  
-
-  /// Generates a server url, we'll assume the server has been started.
-  private func serverURL(path: String = "") -> URL {
-    var components = URLComponents()
-    components.scheme = server.isSecure ? "https" : "http"
-    components.host = "localhost"
-    components.port = Int(server.port)
-    components.path = path
-    return components.url!
-  }
-}
